7f741cfe7ad0891e525e6988efd1165248dae85d
iona-b <ionabrabender@gmail.com>
2020-11-01 15:33:10 -0800

    updated readme

--- src/containers/CreateScheduleContainer.js
+++ src/containers/CreateScheduleContainer.js
@@ -26,7 +26,7 @@ class CreateScheduleContainer extends React.Component {
     return (
       <div> 
         {this.props.user.id && this.props.currentSchedule.id === ""?
-          <div>
+          <div id="create-schedule-container">
             <ScheduleInProgress />
             <CreateScheduleForm updateCurrentSchedule={this.props.updateCurrentSchedule}/>
             <ShowExistingTasks />
@@ -41,7 +41,10 @@ class CreateScheduleContainer extends React.Component {
             </Link>
             <img src={require("../images/background-bottom-left.png")} alt='' id="background-bottom-left" />
             <img src={require("../images/background-top-right.png")} alt='' id="background-top-right" />
-            <div className="info-divs">
+            <div className="info-divs" id="loading-div">
+              <h2 id="loading-text">Loading...</h2>
+            </div>
+            <div className="info-divs" id="view-edit-delete-div">
               <h2>You've already created a schedule for today.</h2>
               <p>You can either view today's schedule or delete your current schedule and create a new one</p>
               <Link to='/schedule' >
